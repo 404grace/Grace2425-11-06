efc92267e909413d19a598f11d5a4b052dd0e8db
LongarMD <mark.longar@gmail.com>
2024-11-13 18:17:47 +0100

    11-06

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
-<script>
+<script lang="ts">
+	import { page } from '$app/stores';
 	import '../app.css';
 </script>
 
-<slot />
+<nav class="w-full p-6 bg-orange-300">
+	<div class="flex gap-4">
+		<a href="/" class={$page.url.pathname === '/' ? 'font-bold' : ''}>Gumb</a>
+		<a href="/domov" class={$page.url.pathname === '/domov' ? 'font-bold' : ''}>Domov</a>
+		<a href="/trgovina" class={$page.url.pathname === '/trgovina' ? 'font-bold' : ''}>Trgovina</a>
+	</div>
+</nav>
+
+<div class="w-full h-screen bg-slate-400">
+	<slot />
+</div>
